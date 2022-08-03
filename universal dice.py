699f95158410ee1d187e70b6d528b3d2b1c8f547
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-08-03 11:13:54 +0530

    Update universal dice.py

--- universal dice.py
+++ universal dice.py
@@ -1,14 +1,18 @@
 a = int(input("Enter a number of players "))
 b = []
-for j in range(a):
-	k = input("Enter a name of player ")
-	b.append(k)
+for j in range(1,a+1):
+    f ="Enter a name of player "+ str(j)
+    k = input(f)
+    b.append(k)
 
 lenth = len(b)
 import random
-
-while 8<9:
-	for ka in range(lenth):
-		print("turn is" ,b[ka])
-		print(random.randint(1,6))
-		input()
+jb = 0
+while jb != 5:
+    for ka in range(lenth):
+        print("turn is " + b[ka],end=' ')
+        print(random.randint(1,6))
+        t = input()
+        
+        if t is str:
+            jb = 5
